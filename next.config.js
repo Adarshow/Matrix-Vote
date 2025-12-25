9c6b4c397a2f52d67020678564223d7a456101a5
Adarsh-Dev-Git <adarshpanakkattu@gmail.com>
2025-12-25 23:14:59 +0530

    Disable ESLint during Vercel builds

--- next.config.js
+++ next.config.js
@@ -3,19 +3,23 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'media.licdn.com',
+        protocol: "https",
+        hostname: "media.licdn.com",
       },
     ],
   },
-}
 
-module.exports = nextConfig
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
