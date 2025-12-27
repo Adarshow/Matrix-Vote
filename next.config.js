773a2b03d7f89067f261d2579f3d1bc3c6af9bf3
Adarsh-Dev-Git <adarshpanakkattu@gmail.com>
2025-12-27 12:39:39 +0530

    Edited next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "media.licdn.com",
       },
+      {
+        protocol: "https",
+        hostname: "gjqyuzbznluyhidoqfjg.supabase.co",
+      },
     ],
   },
 
